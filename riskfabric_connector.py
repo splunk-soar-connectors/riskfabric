c40a5211d408ab02f784ad5e7a0abbf29bf8babd
Matt Tichenor <mtichenor@splunk.com>
2018-08-21 10:19:31 -0500

    Initial commit of Risk Fabric app from Brian Rowe

---
+++ riskfabric_connector.py
@@ -0,0 +1,342 @@
+# -----------------------------------------
+# Risk Fabric Phantom App Connector python file
+# -----------------------------------------
+
+# Phantom App imports
+import phantom.app as phantom
+from phantom.base_connector import BaseConnector
+from phantom.action_result import ActionResult
+
+# Usage of the consts file is recommended
+# from riskfabric_consts import *
+import requests
+import json
+# import base64
+from bs4 import BeautifulSoup
+
+
+class RetVal(tuple):
+    def __new__(cls, val1, val2):
+        return tuple.__new__(RetVal, (val1, val2))
+
+
+class RiskFabricConnector(BaseConnector):
+
+    def __init__(self):
+
+        # Call the BaseConnectors init first
+        super(RiskFabricConnector, self).__init__()
+
+        self._state = None
+        self._header = {"Content-Type": "application/json"}
+        self._base_url = None
+        self._username = None
+        self._password = None
+
+    def _process_empty_reponse(self, response, action_result):
+
+        if response.status_code == 200:
+            return RetVal(phantom.APP_SUCCESS, {})
+
+        return RetVal(action_result.set_status(phantom.APP_ERROR, "Empty response and no information in the header"), None)
+
+    def _process_html_response(self, response, action_result):
+
+        # An html response, treat it like an error
+        status_code = response.status_code
+
+        try:
+            soup = BeautifulSoup(response.text, "html.parser")
+            error_text = soup.text
+            split_lines = error_text.split('\n')
+            split_lines = [x.strip() for x in split_lines if x.strip()]
+            error_text = '\n'.join(split_lines)
+        except:
+            error_text = "Cannot parse error details"
+
+        message = "Status Code: {0}. Data from server:\n{1}\n".format(status_code,
+                error_text)
+
+        message = message.replace('{', '{{').replace('}', '}}')
+
+        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
+
+    def _process_json_response(self, r, action_result):
+
+        # Try a json parse
+        try:
+            resp_json = r.json()
+        except Exception as e:
+            return RetVal(action_result.set_status(phantom.APP_ERROR, "Unable to parse JSON response. Error: {0}".format(str(e))), None)
+
+        # Please specify the status codes here
+        if 200 <= r.status_code < 399:
+            return RetVal(phantom.APP_SUCCESS, resp_json)
+
+        # You should process the error returned in the json
+        message = "Error from server. Status Code: {0} Data from server: {1}".format(
+                r.status_code, r.text.replace('{', '{{').replace('}', '}}'))
+
+        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
+
+    def _process_response(self, r, action_result):
+
+        # store the r_text in debug data, it will get dumped in the logs if the action fails
+        if hasattr(action_result, 'add_debug_data'):
+            action_result.add_debug_data({'r_status_code': r.status_code})
+            action_result.add_debug_data({'r_text': r.text})
+            action_result.add_debug_data({'r_headers': r.headers})
+
+        # Process each 'Content-Type' of response separately
+
+        # Process a json response
+        if 'json' in r.headers.get('Content-Type', ''):
+            return self._process_json_response(r, action_result)
+
+        # Process an HTML response, Do this no matter what the api talks.
+        # There is a high chance of a PROXY in between phantom and the rest of
+        # world, in case of errors, PROXY's return HTML, this function parses
+        # the error and adds it to the action_result.
+        if 'html' in r.headers.get('Content-Type', ''):
+            return self._process_html_response(r, action_result)
+
+        # it's not content-type that is to be parsed, handle an empty response
+        if not r.text:
+            return self._process_empty_reponse(r, action_result)
+
+        # everything else is actually an error at this point
+        message = "Can't process response from server. Status Code: {0} Data from server: {1}".format(
+                r.status_code, r.text.replace('{', '{{').replace('}', '}}'))
+
+        return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
+
+    def _make_rest_call(self, endpoint, action_result, params=None, method="get"):
+
+        resp_json = None
+
+        try:
+            request_func = getattr(requests, method)
+        except AttributeError:
+            return RetVal(action_result.set_status(phantom.APP_ERROR, "Invalid method: {0}".format(method)), resp_json)
+
+        # Create a URL to connect to
+        url = self._base_url + endpoint
+
+        try:
+            r = request_func(url, auth=(self._username, self._password), params=params)
+        except Exception as e:
+            return RetVal(action_result.set_status( phantom.APP_ERROR,
+                          "Error Connecting to server. Url: {0}, Username: {1}, Password: {2}, Details: {3}".format(url,
+                          self._username, self._password, str(e))), resp_json)
+        return self._process_response(r, action_result)
+
+    def _handle_test_connectivity(self, param):
+
+        # Add an action result object to self (BaseConnector) to represent the action for this param
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        # NOTE: test connectivity does _NOT_ take any parameters
+        # i.e. the param dictionary passed to this handler will be empty.
+        # Also typically it does not add any data into an action_result either.
+        # The status and progress messages are more important.
+
+        self.save_progress("Connecting to endpoint")
+        ret_val, response = self._make_rest_call('/restapi/search/entityTypes', action_result)
+
+        if (phantom.is_fail(ret_val)):
+            # the call to the 3rd party device or service failed, action result should contain all the error details
+            # so just return from here
+            self.save_progress("Test Connectivity Failed. Error: {0}".format(action_result.get_message()))
+            return action_result.get_status()
+
+        # Return success
+        self.save_progress("Test Connectivity Passed")
+        return action_result.set_status(phantom.APP_SUCCESS)
+
+    def _handle_get_user_risk_score(self, param):
+
+        # Implement the handler here
+        # use self.save_progress(...) to send progress messages back to the platform
+        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+
+        # Add an action result object to self (BaseConnector) to represent the action for this param
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        # Access action parameters passed in the 'param' dictionary
+        # Required values can be accessed directly
+        queryvalue = param['queryvalue']
+
+        # Optional values should use the .get() function
+        # optional_parameter = param.get('', 'default_value')
+        params = [('query', queryvalue), ('entityType', 'user'), ('fields', 'riskinfo')]
+        ret_val, response = self._make_rest_call('/restapi/search', action_result, params=params)
+
+        if (phantom.is_fail(ret_val)):
+            # the call to the 3rd party device or service failed, action result should contain all the error details
+            # so just return from here
+            return action_result.get_status()
+
+        # Now post process the data,  uncomment code as you deem fit
+        action_result = self.parse_risk_score(response, action_result)
+
+        # Return success, no need to set the message, only the status
+        # BaseConnector will create a textual message based off of the summary dictionary
+        return action_result.set_status(phantom.APP_SUCCESS)
+
+    def _handle_get_ip_risk_score(self, param):
+
+        # Implement the handler here
+        # use self.save_progress(...) to send progress messages back to the platform
+        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+
+        # Add an action result object to self (BaseConnector) to represent the action for this param
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        # Access action parameters passed in the 'param' dictionary
+        # Required values can be accessed directly
+        queryvalue = param['queryvalue']
+
+        # Optional values should use the .get() function
+        # optional_parameter = param.get('', 'default_value')
+        params = [('query', queryvalue), ('entityType', 'ipaddress'), ('fields', 'riskinfo')]
+        ret_val, response = self._make_rest_call('/restapi/search', action_result, params=params)
+
+        if (phantom.is_fail(ret_val)):
+            # the call to the 3rd party device or service failed, action result should contain all the error details
+            # so just return from here
+            return action_result.get_status()
+
+        # Now post process the data,  uncomment code as you deem fit
+        action_result = self.parse_risk_score(response, action_result)
+
+        # Return success, no need to set the message, only the status
+        # BaseConnector will create a textual message based off of the summary dictionary
+        return action_result.set_status(phantom.APP_SUCCESS)
+
+    def _handle_get_hostname_risk_score(self, param):
+
+        # Implement the handler here
+        # use self.save_progress(...) to send progress messages back to the platform
+        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+
+        # Add an action result object to self (BaseConnector) to represent the action for this param
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        # Access action parameters passed in the 'param' dictionary
+        # Required values can be accessed directly
+        queryvalue = param['queryvalue']
+
+        # Optional values should use the .get() function
+        # optional_parameter = param.get('', 'default_value')
+        params = [('query', queryvalue), ('entityType', 'computerendpoint'), ('fields', 'riskinfo')]
+        ret_val, response = self._make_rest_call('/restapi/search', action_result, params=params)
+
+        if (phantom.is_fail(ret_val)):
+            # the call to the 3rd party device or service failed, action result should contain all the error details
+            # so just return from here
+            return action_result.get_status()
+
+        # Now post process the data,  uncomment code as you deem fit
+        action_result = self.parse_risk_score(response, action_result)
+
+        # Return success, no need to set the message, only the status
+        # BaseConnector will create a textual message based off of the summary dictionary
+        return action_result.set_status(phantom.APP_SUCCESS)
+
+    def parse_risk_score(self, response, action_result):
+
+        respRiskScores = []
+        respRiskRatings = []
+
+        for respdata in response['Data']:
+
+            # Add the response into the data section
+            try:
+                respRiskOverview = respdata['RiskInfo']['Overview']
+                respRiskScore = respRiskOverview['RiskScore']
+                respRiskRating = respRiskOverview['RiskRating']
+            except:
+                respRiskScore = 'None'
+                respRiskRating = 'None'
+
+            response_dict = {'entity_name': respdata['EntityName'],
+                        'entity_type': respdata['EntityType'],
+                        'risk_score': respRiskScore,
+                        'risk_rating': respRiskRating}
+
+            action_result.add_data(response_dict)
+            respRiskScores.append(respRiskScore)
+            respRiskRatings.append(respRiskRating)
+
+        # Add a dictionary that is made up of the most important values from data into the summary
+        summary = action_result.update_summary({})
+        summary['risk_scores'] = respRiskScores
+        summary['risk_ratings'] = respRiskRatings
+
+        return action_result
+
+    def handle_action(self, param):
+
+        ret_val = phantom.APP_SUCCESS
+
+        # Get the action that we are supposed to execute for this App Run
+        action_id = self.get_action_identifier()
+
+        self.debug_print("action_id", self.get_action_identifier())
+
+        if action_id == 'test_connectivity':
+            ret_val = self._handle_test_connectivity(param)
+
+        elif action_id == 'get_user_risk_score':
+            ret_val = self._handle_get_user_risk_score(param)
+
+        elif action_id == 'get_ip_risk_score':
+            ret_val = self._handle_get_ip_risk_score(param)
+
+        elif action_id == 'get_hostname_risk_score':
+            ret_val = self._handle_get_hostname_risk_score(param)
+
+        return ret_val
+
+    def initialize(self):
+
+        # Load the state in initialize, use it to store data
+        # that needs to be accessed across actions
+        self._state = self.load_state()
+
+        config = self.get_config()
+
+        self._base_url = config.get("baseurl")
+        self._username = config.get("username")
+        self._password = config.get("password")
+
+        return phantom.APP_SUCCESS
+
+    def finalize(self):
+
+        # Save the state, this data is saved across actions and app upgrades
+        self.save_state(self._state)
+        return phantom.APP_SUCCESS
+
+
+if __name__ == '__main__':
+
+    import sys
+    import pudb
+    pudb.set_trace()
+
+    if (len(sys.argv) < 2):
+        print "No test json specified as input"
+        exit(0)
+
+    with open(sys.argv[1]) as f:
+        in_json = f.read()
+        in_json = json.loads(in_json)
+        print(json.dumps(in_json, indent=4))
+
+        connector = RiskFabricConnector()
+        connector.print_progress_message = True
+        ret_val = connector._handle_action(json.dumps(in_json), None)
+        print (json.dumps(json.loads(ret_val), indent=4))
+
+    exit(0)
